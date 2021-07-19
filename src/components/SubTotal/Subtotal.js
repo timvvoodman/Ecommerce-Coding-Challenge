88fe6e651e54773aab20ebe9c3b9515f7c597ae3
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-07-18 22:56:52 -0400

    added currency format react to resolve currency format issues

--- src/components/SubTotal/Subtotal.js
+++ src/components/SubTotal/Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Subtotal.css'
 import { useStateValue } from '../../Context/AppState'
 import { getCartTotal } from '../../Context/reducer'
+import CurrencyFormat from 'react-currency-format'
 import './Subtotal.css'
 
 function Subtotal() {
@@ -11,10 +12,19 @@ function Subtotal() {
 
   return (
     <div className="subtotal">
-      <p>
-        Subtotal ({cart.length} items):
-        <strong> ${cartTotal.toFixed(2)}</strong>
-      </p>
+      <CurrencyFormat
+        renderText={(value) => (
+          <p>
+            Subtotal ({cart.length} items):
+            <strong> {value}</strong>
+          </p>
+        )}
+        decimalScale={2}
+        value={cartTotal}
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={'$'}
+      />
 
       <button>Proceed to Checkout</button>
     </div>
